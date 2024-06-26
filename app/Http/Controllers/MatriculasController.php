0b6011b970081cf948ff56f164381079c8b83e98
fqueta <ger.maisaqui1@gmail.com>
2024-06-26 05:35:47 -0300

    26/06/2024 01

--- app/Http/Controllers/MatriculasController.php
+++ app/Http/Controllers/MatriculasController.php
@@ -208,7 +208,7 @@ class MatriculasController extends Controller
                         $v['data_solicit_certificado'],
                         $v['data_blacklist'],
                         $v['data_seguir'],
-                        $v['data_inicio'],
+                        // $v['data_inicio'],
                         $v['data_documento'],
                         // $v['data_contrato'],
                         $v['data_matricula'],
@@ -226,7 +226,7 @@ class MatriculasController extends Controller
                     $v['data_situacao'] = date('Y-m-d H:i:s');
                     // $v['data_matricula'] = date('Y-m-d H:i:s');
                     $v['data_agendamento'] = date('Y-m-d');
-                    // $v['data_inicio'] = date('Y-m-d H:i:s');
+                    $v['data_inicio'] = date('Y-m-d H:i:s');
                     $v['data_contrato'] = date('Y-m-d H:i:s');
                     $v['tag_sys'] = Qlib::lib_array_json(['add_sisema']);
                     $v['historico'] = Qlib::lib_array_json([
